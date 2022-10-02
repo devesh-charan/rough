75923373db59ad1968d01e046fd821f7cdfdbf13
Devesh Charan <devesh.charan2020@vitbhopal.ac.in>
2022-10-03 02:10:26 +0530

    Add files via upload

---
+++ prime_number_finder.py
@@ -0,0 +1,7 @@
+for n in range(2,10):
+    for x in range(2,n):
+        if n%x ==0:
+            print(n,'equal',x,'*',n//x)
+            break
+        else:
+            print(n,"is a prime number")
